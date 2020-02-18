3775405b849386e4082186f17f409a59626fc13a
Filip Kin <filip@kinmails.com>
2020-02-17 21:58:20 -0500

    updated for st joe

--- js/constants.js
+++ js/constants.js
@@ -2,5 +2,5 @@ apiBaseUrl='https://www.thebluealliance.com/api/v3';
 tbaApiKey='zz0S5gjhuFGRiHdJSIeQCIiPHWRi8HeGT6ox7MLsvnjc2BdrnfQw3o7Ll9pAPMdF'; // Get an API key at https://www.thebluealliance.com/request/apiwrite
 updateRate=15; // Seconds between data refreshes
 teamId='frc4384'; // Don't forget to prefix with "frc"
-eventId='2019mial2'; // Just go to the blue alliance page for the event, it's in the url ex: https://www.thebluealliance.com/event/2019mial2
+eventId='2020misjo'; // Just go to the blue alliance page for the event, it's in the url ex: https://www.thebluealliance.com/event/2019mial2
 resetTime=8; // Time between matches
